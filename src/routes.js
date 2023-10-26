b43dcd9f63fe5b2addd000e8fb65444b43421802
MatiLafor16 <matilaforcada@gmail.com>
2023-10-26 00:12:13 -0300

    actualicé el archivo routes.js
    añadí la ruta de delete por isbn y por id

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ router.get('/libro', libro.getOne)   //para uno
 
 router.post('/libro', libro.add); //añadir libro
 
-router.delete('/libro', libro.delete); //eliminar libro
+router.delete('/libroISBN', libro.deleteISBN); //eliminar libro por ISBN
+
+router.delete('/libroID', libro.deleteID); //eliminar libro por ID
 
 router.put('/libro', libro.update); //actualizar libro
