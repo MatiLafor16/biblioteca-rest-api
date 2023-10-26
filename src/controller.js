78eeb21d046c83bc6383e6cd887c71bfc88ddde0
MatiLafor16 <matilaforcada@gmail.com>
2023-10-25 22:48:13 -0300

    añadi mas metodos
    añadí los metodos de agregar
    eliminar
    actualizar
    borrar

--- src/controller.js
+++ src/controller.js
@@ -14,5 +14,11 @@ class LibroController{
         res.json(result);
     }
 
+    async add(req,res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, yearpublicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.yearpublicacion, libro.ISBN]);
+        res.json({"Se ha añadido un nuevo libro": result.insertId});
+    }    
+
 }
 export const libro = new LibroController();
--- src/routes.js
+++ src/routes.js
@@ -7,3 +7,9 @@ export const router = Router();
 router.get('/libros', libro.getAll);  //para todos
 
 router.get('/libro', libro.getOne)   //para uno
+
+router.post('/libro', libro.add); //añadir libro
+
+router.delete('/libro', libro.delete); //eliminar libro
+
+router.put('/libro', libro.update); //actualizar libro
